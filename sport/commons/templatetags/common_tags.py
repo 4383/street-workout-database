a83a0e0a17c4e90806b797badbfb61106cbaeb1c
herve beraud <herve.beraud@openmailbox.org>
2015-08-16 14:29:16 +0200

    Exercises main menu don't list all categories, just offer categories, and muscles overview links

--- sport/commons/templatetags/common_tags.py
+++ sport/commons/templatetags/common_tags.py
@@ -2,12 +2,14 @@ __author__ = 'herve.beraud'
 from django import template
 from exercises.models import Category
 from exercises.models import MuscleGroup
+from exercises.models import Muscle
 
 register = template.Library()
 
 
 @register.inclusion_tag('common_tags/show_exercises_menu.html')
 def show_exercises_menu():
-    categories = Category.objects.filter(active=True)
-    muscles_group = MuscleGroup.objects.filter(active=True)
-    return {'categories': categories, 'muscles_group': muscles_group}
+    categories = Category.objects.filter(active=True).count()
+    muscles_groups = MuscleGroup.objects.filter(active=True).count()
+    muscles = Muscle.objects.filter(active=True).count()
+    return {'categories': categories, 'muscles_group': muscles_groups, 'muscles': muscles}
