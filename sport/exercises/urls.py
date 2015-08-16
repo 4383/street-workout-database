e903df1160d4d3d2414de570316203926e22f7c5
herve beraud <herve.beraud@openmailbox.org>
2015-08-16 14:28:08 +0200

    Add category details page

--- sport/exercises/urls.py
+++ sport/exercises/urls.py
@@ -4,7 +4,15 @@ from django.conf.urls import url, patterns
 from . import views
 
 urlpatterns = [
+    url(r'^exercise/(?P<slug>[a-z\-0-9]+)/$', views.exercise, name='exercise'),
+    # Categories
     url(r'^categories/$', views.categories, name='exercises_categories'),
     url(r'^category/(?P<slug>[a-z\-0-9]+)/$', views.category, name='exercises_category'),
-    url(r'^exercise/(?P<slug>[a-z\-0-9]+)/$', views.exercise, name='exercise'),
+    # Muscles
+    url(r'^muscles/$', views.muscles, name='exercises_muscles'),
+    url(r'^muscle/(?P<slug>[a-z\-0-9]+)/$', views.muscle, name='exercises_muscle'),
+    # Muscles-Groups
+    url(r'^muscles-groups/$', views.muscles_groups, name='exercises_muscles_groups'),
+    url(r'^muscle-group/(?P<slug>[a-z\-0-9]+)/$', views.muscle_group, name='exercises_muscle_group'),
 ]
+
