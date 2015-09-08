17ae38b6529e8317f7acf0b5bda945be2e5b394c
herve beraud <herve.beraud@openmailbox.org>
2015-09-08 21:25:39 +0200

    Add common templatetags video gallery and information messages

--- sport/commons/templatetags/common_tags.py
+++ sport/commons/templatetags/common_tags.py
@@ -1,5 +1,8 @@
 __author__ = 'herve.beraud'
+from datetime import datetime, timedelta
 from django import template
+from django.core.exceptions import ObjectDoesNotExist
+from community.models import InformationMessage
 from exercises.models import Category
 from exercises.models import MuscleGroup
 from exercises.models import Muscle
@@ -18,3 +21,30 @@ def show_exercises_menu():
 @register.inclusion_tag('common_tags/image_gallery.html')
 def images_gallery(images):
     return {"images": images}
+
+
+@register.inclusion_tag('common_tags/video_gallery.html')
+def videos_gallery(videos):
+    return {"videos": videos}
+
+
+@register.inclusion_tag('common_tags/display_information_message.html', takes_context=True)
+def display_information_message(context):
+    expiration_date = datetime.today() + timedelta(days=365)
+    cookie_date_format = "%a, %d %b %Y %I:%M:%S GMT"
+    try:
+        information_message = InformationMessage.objects.filter(
+            active=True,
+            display_date__lte=datetime.now(), expiration_date__gt=datetime.now()).latest('publish_date')
+        request = context['request']
+        if information_message.display_once:
+            try:
+                already_read_information_message_id = int(request.COOKIES.get('information_message_id'))
+                if already_read_information_message_id == information_message.id:
+                    information_message = None
+            # Cookie not found
+            except TypeError:
+                pass
+    except ObjectDoesNotExist:
+        information_message = None
+    return {"information_message": information_message, "expiration_date": expiration_date.strftime(cookie_date_format)}
