627d82a8c2459264bf0cbc42433fe1b68d073d2d
herve beraud <herve.beraud@openmailbox.org>
2015-11-24 21:20:57 +0100

    [feat] index, about, muscles

--- sport/home/views.py
+++ sport/home/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from exercises.models import Exercise
+
 
 # Create your views here.
+def about(request):
+    return render(request, "about.html")
+
+
 def index(request):
-    return render(request, "index.html")
+    exercises_number = Exercise.objects.filter(active=True).count()
+    context = {"exercises_number": exercises_number}
+    return render(request, "index.html", context)
--- sport/sport/urls.py
+++ sport/sport/urls.py
@@ -3,13 +3,15 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from home import views as home_view
+from exercises import views as exercises_view
 
 urlpatterns = [
     url(r"^admin/", include(admin.site.urls)),
     url(r"^exercises/", include('exercises.urls')),
     url(r"^/$", home_view.index, name="index"),
     url(r"^$", home_view.index, name="index"),
+    url(r"^about/$", home_view.about, name="about"),
+    url(r"^about$", home_view.about, name="about"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-#urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
