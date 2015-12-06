b027cbfe15ed30c18ec8529ddc7bbf57a853ecb1
herve beraud <herve.beraud@openmailbox.org>
2015-12-06 23:14:54 +0100

    [chore] maintain compatibility python versions

--- sport/web/sport/settings.py
+++ sport/web/sport/settings.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-import configparser
+try:
+    import configparser
+except ImportError:
+    import ConfigParser as configparser
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
