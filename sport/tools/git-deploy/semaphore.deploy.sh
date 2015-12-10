0f5d7048c7866535d5b8ee945d6175f146d5598c
herve beraud <herve.beraud@openmailbox.org>
2015-12-10 23:54:32 +0100

    [deploy] revert rename branch

--- sport/tools/git-deploy/semaphore.deploy.sh
+++ sport/tools/git-deploy/semaphore.deploy.sh
@@ -5,4 +5,4 @@ scp -P <port> sport/tools/git-deploy/post-update.sh staging@<host>:/home/staging
 ssh -p <port> staging@<host> 'chmod +x /home/staging/git/staging.swd.git/hooks/post-update'
 git remote rm hub || true
 git remote add hub  ssh://staging@<host>:<port>/home/staging/git/swd.git || true
-git push --force hub staging || true
+git push --force hub $BRANCH_NAME || true
