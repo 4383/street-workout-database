e1a429cf8e55537ceeb3af0e5f7a48e09932682a
herve beraud <herve.beraud@openmailbox.org>
2015-12-10 22:56:13 +0100

    [deploy] change git hook mode for automatic execution

--- sport/tools/git-deploy/semaphore.deploy.sh
+++ sport/tools/git-deploy/semaphore.deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 ssh-keyscan -H -p <port> <host> >> ~/.ssh/known_hosts
 echo -e "Host *\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+scp -P <port> sport/tools/git-deploy/post-update.sh staging@<host>:/home/staging/git/staging.swd.git/hooks/post-update
+ssh -p <port> staging@<host> 'chmod +x /home/staging/git/staging.swd.git/hooks/post-update'
 git remote rm staging || true
 git remote add staging  ssh://staging@<host>:<port>/home/staging/git/swd.git || true
 git push --force staging $BRANCH_NAME || true
