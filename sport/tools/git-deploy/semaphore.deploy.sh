f90c1c5dabeed6954662526793bb17b6f4415277
herve beraud <herve.beraud@openmailbox.org>
2015-12-11 19:41:05 +0100

    [deploy] remove before install

--- sport/tools/git-deploy/semaphore.deploy.sh
+++ sport/tools/git-deploy/semaphore.deploy.sh
@@ -3,6 +3,7 @@ ssh-keyscan -H -p <port> <host> >> ~/.ssh/known_hosts
 echo -e "Host *\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 scp -P <port> sport/tools/git-deploy/post-update.sh staging@<host>:/home/staging/git/staging.swd.git/hooks/post-update
 ssh -p <port> staging@<host> 'chmod +x /home/staging/git/staging.swd.git/hooks/post-update'
+ssh -p <port> staging@<host> 'rm -rf /home/staging/projects/swd'
 git remote rm hub || true
 git remote add hub  ssh://staging@<host>:<port>/home/staging/git/swd.git || true
 git push --force hub $BRANCH_NAME:staging || true
