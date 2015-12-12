9809cb6f353b6ecedb854325f8886328caede2dc
herve beraud <herve.beraud@openmailbox.org>
2015-12-12 23:17:40 +0100

    [fix] git-deploy debugging

--- sport/tools/git-deploy/post-update.sh
+++ sport/tools/git-deploy/post-update.sh
@@ -15,22 +15,27 @@ SOCKETDIR=/home/`whoami`/sockets/swd
 WWWDIR=/home/`whoami`/www/swd
 DJANGODIR=$PROJECTDIR/street-workout-database/sport/web
 GUNICORN=$PROJECTDIR/$PROJECT/sport/tools/server/gunicorn/gunicorn.sh
+ENVNAME=`whoami`
 
 function rollback () {
+    echo "rollback require" >> $LOG_FILE
     git reset $CURRENT_REVISION
     $PROJECTDIR/bin/python manage.py collectstatic --clear --noinput
     $PROJECTDIR/bin/python manage.py makemigrations
     $PROJECTDIR/bin/python manage.py migrate
     chmod +x $GUNICORN
     supervisorctl start swd_`whoami`
+    echo "rollback ok" >> $LOG_FILE
 }
 
-echo "**** Pulling changes into Live [Hub's post-update hook]" >> $LOG_FILE
+echo "**** Pulling changes into Live [Hub's post-update hook]" > $LOG_FILE
 echo "Starting at: `date`" >> $LOG_FILE
 
 supervisorctl stop swd_`whoami`
+echo `supervisorctl status swd_$ENVNAME` >> $LOG_FILE
 
 if [ ! -d "$PROJECTDIR" ]; then
+    echo "create virtualenv" >> $LOG_FILE
     virtualenv-3.2 $PROJECTDIR
 fi
 
@@ -47,6 +52,7 @@ fi
 
 cd $PROJECTDIR/$PROJECT || exit
 CURRENT_REVISION=`git rev-parse HEAD`
+echo "Last current revision $CURRENT_REVISION" >> $LOG_FILE
 
 cd $PROJECTDIR
 rm -rf $PROJECTDIR/$PROJECT
@@ -76,13 +82,13 @@ fi
 
 chmod +x $GUNICORN
 supervisorctl start swd_`whoami`
-ERROR_OCCURED=`supervisorctl status swd_\`whoami\` | grep 'STOPPED'`
+ERROR_OCCURED=`supervisorctl status swd_$ENVNAME | grep 'STOPPED'`
+echo "supervisor status : $ERROR_OCCURED" >> $LOG_FILE
 
 # rollback if supervirsor start fail
 if [ ! -z $ERROR_OCCURED ]; then
-    if [ $? -eq 1 ]; then
-        rollback
-    fi
+    echo "Supervisor is done but expected to start" >> $LOG_FILE
+    rollback
 fi
 
 echo "Finish at: `date`" >> $LOG_FILE
